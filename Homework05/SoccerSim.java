eb40877d711759bb61e93fb3358e7071b091e6eb
cgreen18 <35274689+cgreen18@users.noreply.github.com>
2018-03-21 15:34:44 -0700

    Decent version of SoccerSim.java

--- Homework05/SoccerSim.java
+++ Homework05/SoccerSim.java
@@ -5,29 +5,104 @@
 
 public class SoccerSim{
 
+  public static final FIELD_X_LEN = 1000;
+  public static final FIELD_Y_LEN = 500;
 
   public SoccerSim(){
 
 
   }
 
-  private String validateArgs(String inputArgs[]){
+  private String[] validateArgs(String inputArgs[]) throws NumberFormatException, IllegalArgumentException {
 
     if (inputArgs.length == 0){   //checks there are arguments
       throw new IllegalArgumentException("Enter arguments.");
     }
-    elseif (inputArgs.length % 4 != 0 || inputArgs.length % 4 != 1){  //if not possible number of arguments entered
+    elseif (inputArgs.length % 4 != 0 && inputArgs.length % 4 != 1){  //if not possible number of arguments entered
       throw new IllegalArgumentException("Enter the arguments as sets of four numbers, possibly followed by a single \ni.e. java SoccerSim 2 23 34 3 .7")
     }
 
-    if (){
-
+    if (inputArgs.length % 4 == 0){  //no timeSlice
+      for (int i = 0; i < inputArgs.length; i += 2){
+        try{
+          mySim.valiPosArgs(inputArgs[i],intputArgs[i+1]);
+          mySim.valiVelArgs(inputArgs[i+2],intputArgs[i+3]);
+        }
+        catch(IllegalArgumentException iae){
+          throw iae;
+        }
+        catch(NumberFormatException nfe){
+          throw nfe;
+        }
+      }
+    }
+    elseif (inputArgs.length % 4 == 1){  //timeSlice entered
+      for (int i = 0; i < inputArgs.length - 1; i += 2){
+        try{
+          mySim.valiPosArgs(inputArgs[i],intputArgs[i+1]);
+          mySim.valiVelArgs(inputArgs[i+2],intputArgs[i+3]);
+        }
+        catch(IllegalArgumentException iae){
+          throw iae;
+        }
+        catch(NumberFormatException nfe){
+          throw nfe;
+        }
+      }
+      try{
+        mySim.valiTimeSliceArg(inputArgs[inputArgs.length-1]);
+      }
+      catch(IllegalArgumentException iae){
+        throw iae;
+      }
+      catch(NumberFormatException nfe){
+        throw nfe;
+      }
     }
 
-
+    return inputArgs;  //will only return if no exceptions are thrown
   }
 
-  private String validate timeSliceArg(String timeSliceArg){
+  private String valiTimeSliceArg(String timeSliceArg) throws NumberFormatException, IllegalArgumentException{
+    double dblTimeSlice;
+
+    try{
+      dblTimeSlice = Double.parseDouble(timeSliceArg);
+    }
+    catch(NumberFormatException nfe){
+      throw nfe;
+    }
+
+    if (dblTimeSlice <= 0 ){
+      throw new IllegalArgumentException("Enter a positive time slice argument");
+    }
+
+    return timeSliceArg;
+  }
+
+  private String[] valiPosArgs(String xPosArg, String yPosArg) throws NumberFormatException, IllegalArgumentException{
+    double dblXPos;
+    double dblYPos;
+    String[] strMatToReturn = new String[2];
+
+    try{
+      dblXPos = Double.parseDouble(xPosArg);
+      dblYPos = Double.parseDouble(yPosArg);
+    }
+    catch(NumberFormatException nfe){
+      throw nfe;
+    }
+
+    if (dblXPos<= 0 || dblYPos <= 0 || dblXPos > FIELD_X_LEN || dblYPos > FIELD_Y_LEN ){
+      throw new IllegalArgumentException("Enter valid position values");
+    }
+
+    strMatToReturn[0] = xPosArg;
+    strMatToReturn[1] = yPosArg;
+    return strMatToReturn;
+  }
+
+  private String valiVelArgs(String xVelArg, String yVelArg) throws NumberFormatException, IllegalArgumentException{
 
   }
 
